c34e592e8371d205d48f253363fab24ed66b03b6
Dzemik55 <tomasz.rycko@gmail.com>
2023-04-29 17:39:19 +0200

    Added CategoryController, added Index View and Partly create View. Added Bootswatch theme.

---
+++ Projekt/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projekt.Data;
+using Projekt.Models;
+
+namespace Projekt.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly DB_Context _db;
+
+        public CategoryController(DB_Context db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+    }
+}
